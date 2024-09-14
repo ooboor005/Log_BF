7bc3c24d7f7a0d26db4c3cd50dab8ea3c579140e
ooboor005 <62879115+ooboor005@users.noreply.github.com>
2024-09-14 13:01:12 +0700

    Update Log_BF.lua

--- Log_BF.lua
+++ Log_BF.lua
@@ -113,7 +113,7 @@ function GetFruitInU()
     for i,v in pairs(game:GetService("ReplicatedStorage").Remotes["CommF_"]:InvokeServer("getInventoryFruits")) do
         if type(v) == "table" then
             if v ~= nil then
-                if v.Price >= 2300000  then
+                if v.Price >= 100000  then
                     table.insert(ReturnText,string.split(v.Name,"-")[2])
                 end
             end
